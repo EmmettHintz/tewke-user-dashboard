f152bc9055e89174d9e834ed71ec1828eaf14f3d
EmmettHintz <eahintz@colgate.edu>
2023-08-01 15:55:49 -0400

    project setup - added bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "~/styles/globals.css";
+
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
