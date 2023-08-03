96ca937196d14b89990cfb0fdf1e9a3018876660
EmmettHintz <eahintz@colgate.edu>
2023-08-03 00:54:20 -0400

    graph component created and added to landing page

---
+++ src/components/Graph.tsx
@@ -0,0 +1,12 @@
+const Graph = () => {
+  return (
+    <div>
+      <p className="lead text-decoration-underline ">
+        This is a placeholder for where I will use chartjs for charts using user
+        data
+      </p>
+    </div>
+  );
+};
+
+export default Graph;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./index.module.css";
 import Head from "next/head";
 import Link from "next/link";
+import Graph from "~/components/Graph";
 
 export default function Home() {
   return (
@@ -10,18 +11,20 @@ export default function Home() {
         <meta name="Dashboard For Your Devices" content="Tewke" />
         <link rel="icon" href="/tewke-logo.jpeg" />
         <title>Hello, world!</title>
-  </Head>
-  <body>
-    <header className="py-5 bg-secondary text-center">
-      <h1 className="display-1">Tewke User Dashboard</h1>
-      <p className="lead mb-0">Here are your products from our line!</p>
-    </header>
+      </Head>
 
-    
+      <header className="bg-secondary py-5 text-center">
+        <h1 className="display-1">Tewke User Dashboard</h1>
+        <p className="lead mb-0">Here are your products from our line!</p>
+      </header>
 
-
-
-  </body>
+      <div className="py-5">
+        <div className="h-100  align-items-center py-5 text-center">
+          <div className="col-lg-6">
+            <Graph />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
